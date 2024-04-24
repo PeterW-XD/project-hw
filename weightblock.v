7b63813e0c7adccb4b8c1aba3bac049e022c8fbb
matheucampbell <matheu.campbell.us@outlook.com>
2024-04-24 17:18:50 -0400

    added weightblock.v

---
+++ weightblock.v
@@ -0,0 +1,21 @@
+module weightblock(
+	input logic clk,
+	input logic reset,
+	input logic detectdone,
+
+	input logic [9:0] maxbin,
+	input logic [9:0] rdaddr2,
+	input logic [9:0] rdaddr3,
+	input logic [9:0] rdaddr4,
+	input logic [23:0] ram1q,
+	input logic [23:0] ram2q,
+	input logic [23:0] ram3q,
+	input logic [23:0] ram4q,
+	
+	output logic done,
+	output logic [7:0] doa,		// signed (-90 to 90)
+);
+
+input logic [3:0] fftvals [27:0];
+
+endmodule
