24b39a65b1c7696a85f11f89606fc3fd71bce644
matheucampbell <matheu.campbell.us@outlook.com>
2024-04-30 00:39:25 -0400

    actually works this time + testbench added

--- weightblock.sv
+++ weightblock.sv
@@ -14,8 +14,7 @@ module weightblock(
 	output logic [9:0] rdaddr4,
 	output logic done,
 	output logic [3:0] bnum,		// (0 to 12)
-	output logic signed [7:0] doa	// (-90 to 90)
-	
+	output logic signed [7:0] doa,	// (-90 to 90)
 );
 
 enum logic [4:0] {idle, memread, micloop, compare, complete} state;
@@ -25,11 +24,12 @@ logic [3:0] maxbnum;		// Beam corresponding to max power
 logic [5:0] dladdr;			// ROM address to get delay coefficient
 logic [27:0] dcoeff;		// Delay coefficient from ROM
 logic [55:0] delayprod;		// Product of FFT and delay coefficient
-logic [55:0] realsq;		// real(delayprod)^2
-logic [55:0] imagsq; 		// imag(delayprod)^2
-logic [56:0] prodmagsq;		// Squared magnitude of delayprod
-logic [60:0] sigpwr;		// Array output for a direction
-logic [60:0] maxpwr;		// Max array output power
+logic signed [31:0] ssreal;	// real(sigsum); sigsum = sum of all delayprods for a given bnum
+logic signed [31:0] ssimag; // imag(sigsum)
+logic [63:0] ssrealsq;		// real(sigsum)^2
+logic [63:0] ssimagsq;		// imag(sigsum)^2
+logic [64:0] sigpwr;		// |sigsum|^2, i.e. array output power
+logic [64:0] maxpwr;		// Max array output power
 logic signed [27:0] sspec [3:0];	// FFTs by channel at maxbin
 
 // Note: rdaddr1 is set to maxbin by freqdetect block while it is in its complete state
@@ -38,7 +38,7 @@ assign rdaddr3 = maxbin;
 assign rdaddr4 = maxbin;
 assign dladdr = 4*bnum + mnum;
 assign doa = -90 + 15*maxbnum;
-assign prodmagsq = realsq + imagsq;
+assign sigpwr = ssrealsq + ssimagsq;
 
 // Delay matrix preloaded into ROM
 delay_ROM drom (
@@ -57,16 +57,16 @@ compmult cmult (
 	.result_imag	(delayprod[27:0])
 );
 
-// Multiplier IP computes square for real(delayprod)^2
+// Multiplier IP computes square for real(sigsum)^2
 realmult m1 (
-	.dataa	(delayprod[55:28]),
-	.result	(realsq)
+	.dataa	(ssreal),
+	.result	(ssrealsq)
 );
 
-// Multiplier IP computes square for imag(delayprod)^2
+// Multiplier IP computes square for imag(sigsum)^2
 realmult m2 (
-	.dataa	(delayprod[27:0]),
-	.result	(imagsq)
+	.dataa	(ssimag),
+	.result	(ssimagsq)
 );
 
 always_ff @(posedge clk) begin
@@ -74,7 +74,8 @@ always_ff @(posedge clk) begin
 		sspec[3:0] <= {ramq4, ramq3, ramq2, ramq1};
 		maxpwr <= 28'b0;
 		maxbnum <= 4'b0;
-		sigpwr <= 28'b0;
+		ssreal <= 32'b0;
+		ssimag <= 32'b0;
 		{mnum, bnum} <= 7'b0;
 		rcount <= 2'b0;
 		done <= 0;
@@ -95,7 +96,8 @@ always_ff @(posedge clk) begin
 			end
 			micloop: begin	// Loop through channels and multiply by dcoeffs
 				// Multipliers work combinationally
-				sigpwr <= sigpwr + prodmagsq;
+				ssreal <= ssreal + {{4{delayprod[55]}}, delayprod[55:28]};
+				ssimag <= ssimag + {{4{delayprod[27]}}, delayprod[27:0]};
 				if (mnum == 3)	begin
 					state <= compare;
 				end else begin
@@ -115,7 +117,8 @@ always_ff @(posedge clk) begin
 				end else begin
 					bnum <= bnum + 1;
 					mnum <= 0;
-					sigpwr <= 0;
+					ssreal <= 32'b0;
+					ssimag <= 32'b0;
 					state <= memread;
 				end
 			end
@@ -123,9 +126,10 @@ always_ff @(posedge clk) begin
 				done <= 0;
 				if (detectdone) begin
 					sspec[3:0] <= {ramq4, ramq3, ramq2, ramq1};
-					maxpwr <= 28'b0;
+					maxpwr <= 65'b0;
 					maxbnum <= 4'b0;
-					sigpwr <= 28'b0;
+					ssreal <= 32'b0;
+					ssimag <= 32'b0;
 					{mnum, bnum} <= 7'b0;
 					rcount <= 0;
 					state <= memread;
---
+++ weightblock_tb.sv
@@ -0,0 +1,145 @@
+`timescale 1ns / 1ps
+
+module weightblock_tb();
+    parameter CLK_PERIOD = 20; // Clock period in ns
+
+    logic clk;
+    logic reset;
+    logic wren;
+    logic done;
+    logic detectdone;
+    
+    logic [3:0] bnum;
+    logic [7:0] doa;
+    logic [27:0] fft1;
+    logic [27:0] fft2;
+    logic [27:0] fft3;
+    logic [27:0] fft4;
+
+    logic [9:0] rdaddress;
+    logic [9:0] wraddress;
+
+	FFT_RAM ram1 (
+	    .clock		(clk),
+		.rdaddress	(10'd44),
+        .wren       (wren),
+        .wraddress  (wraddress),
+		.data		(fft1),
+        .q          (dut.ramq1)
+	);
+
+    FFT_RAM ram2 (
+	    .clock		(clk),
+		.rdaddress	(dut.rdaddr2),
+        .wren       (wren),
+        .wraddress  (wraddress),
+		.data		(fft2),
+        .q          (dut.ramq2)
+	);
+
+    FFT_RAM ram3 (
+	    .clock		(clk),
+		.rdaddress	(dut.rdaddr3),
+        .wren       (wren),
+        .wraddress  (wraddress),
+		.data		(fft3),
+        .q          (dut.ramq3)
+	);
+
+    FFT_RAM ram4 (
+	    .clock		(clk),
+		.rdaddress	(dut.rdaddr4),
+        .wren       (wren),
+        .wraddress  (wraddress),
+		.data		(fft4),
+        .q          (dut.ramq4)
+	);
+	
+	weightblock dut (
+        .clk        (clk),
+        .reset      (reset),
+        .detectdone (detectdone),
+        .maxbin     (10'd44),
+        .ramq1      (ram1.q),
+        .ramq2      (ram2.q),
+        .ramq3      (ram3.q),
+        .ramq4      (ram4.q),
+
+        .rdaddr2    (ram2.rdaddress),
+        .rdaddr3    (ram3.rdaddress),
+        .rdaddr4    (ram4.rdaddress),
+        .done       (done),
+        .bnum       (bnum),
+        .doa        (doa),
+    );
+
+	always #((CLK_PERIOD / 2)) clk = ~clk;
+
+    initial begin
+        clk = 1;
+        reset = 0;
+        wren = 0;
+        done = 0;
+        detectdone = 0;
+        bnum = 4'b0;
+        doa = 8'b0;
+        rdaddress = 10'd44;
+        wraddress = 10'd44;
+
+        // 60 AOA
+        fft1[27:14] = -14'd226;
+        fft1[13:0] = -14'd310;
+        fft2[27:14] = 14'd338;
+        fft2[13:0] = -14'd175;
+        fft3[27:14] = -14'd175;
+        fft3[13:0] = -14'd557;
+        fft4[27:14] = 14'd156;
+        fft4[13:0] = 14'd501;
+
+        // 60 AOA
+        // fft1[27:14] = -14'd212;
+        // fft1[13:0] = -14'd394;
+        // fft2[27:14] = 14'd391;
+        // fft2[13:0] = 14'd284;
+        // fft3[27:14] = -14'd255;
+        // fft3[13:0] = -14'd208;
+        // fft4[27:14] = 14'd299;
+        // fft4[13:0] = 14'd513;
+
+        // -45 AOA but algorithm finds 75 (-15 SNR)
+        // fft1[27:14] = -14'd365;
+        // fft1[13:0] = -14'd244;
+        // fft2[27:14] = 14'd106;
+        // fft2[13:0] = 14'd509;
+        // fft3[27:14] = 14'd357;
+        // fft3[13:0] = -14'd468;
+        // fft4[27:14] = -14'd228;
+        // fft4[13:0] = 14'd306;
+
+        // Fill RAM
+        wren = 1;
+        #40;
+        wren = 0;
+
+        // Reset
+        reset = 1;
+        #40;
+        reset = 0;
+
+        // Trigger weightblock to start
+        detectdone = 1;
+        #20;
+        detectdone = 0;
+
+        #10000;
+
+        // Test reset behavior
+        detectdone = 1;
+        #20;
+        detectdone = 0;
+
+        #5000;
+        $stop;
+    end
+
+endmodule
